a2f2232883e3f5c5be73bbb740ca50aa5054b848
Taimoor-Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2021-02-20 18:47:42 -0500

    Added lower third overlay

--- app/src/overlay_routes.js
+++ app/src/overlay_routes.js
@@ -30,15 +30,36 @@ router.route('/overlayID/:postID?')
         else {
             switch (req.params.postID.toLowerCase()) {
                 case "test":
-                    io.emitTo("overlayID", "test", "THIS IS A TEST")
-                        .then(() => {
-                            res.send(`Event sent`);
-                        })
-                        .catch(e => {
-                            res.send(`Error: "${e}"`);
-                        });
-                    return;
+                    return io.emitTo("overlayID", "test", "THIS IS A TEST")
+                        .then(() => { res.send(`Event sent`) })
+                        .catch(e => { res.send(`Error: "${e}"`) });
 
+                default:
+                    return res.send(`Handel request for ${req.params.postID}`);                
+            }
+        }
+    });
+
+
+router.route('/Lower-Third/:postID?')
+    .get(async (req, res) => {
+        res.send(pug.renderFile(`${__dirname}/views/pug/Lower-Third.overlay.pug`, {
+            style: sass.renderSync({file: `${__dirname}/views/scss/Lower-Third.overlay.scss`}).css.toString(),
+        }))
+    })
+    .post(async (req, res) => {
+        if (!req.params.postID) res.send("No event specified");
+        else {
+            switch (req.params.postID.toLowerCase()) {
+                case "visibility":
+                    return io.emitTo("Lower-Third", "visibility", req.body.value)
+                        .then(() => { res.send(`Event sent`) })
+                        .catch(e => { res.send(`Error: "${e}"`) });
+                case "update":
+                    return io.emitTo("Lower-Third", "update", req.body)
+                        .then(() => { res.send(`Event sent`) })
+                        .catch(e => { res.send(`Error: "${e}"`) });
+    
                 default:
                     return res.send(`Handel request for ${req.params.postID}`);                
             }
